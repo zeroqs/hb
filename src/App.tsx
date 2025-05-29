2bb6651eca5657e90dd335c476337ac5cae88bf1
zeroqs <smthq@bk.ru>
2025-05-29 22:21:13 +0300

    gg

--- src/App.tsx
+++ src/App.tsx
@@ -21,9 +21,9 @@ function App() {
     <div>
       <Confetti width={windowSize.width} height={windowSize.height} />
 
-      <h1 style={{ fontSize: "3rem", color: "#ff69b4" }}>
-        С Днём Рождения! 🎂 🎉
-      </h1>
+      <h1 style={{ fontSize: "3rem", color: "#ff69b4" }}>С Днём Рождения!</h1>
+      <h1 style={{ fontSize: "3rem", color: "#ff69b4" }}>🎂 🎉</h1>
+
       <p style={{ fontSize: "1.2rem", marginTop: "2rem", color: "#ff69b4" }}>
         🎁 Желаю радости, тепла и вдохновения каждый день! 🧁
       </p>
